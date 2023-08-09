00e611377fb02d7ef5e81fc5047d8bb1f7b9fdc8
Danil Topchii <mexehamy@gmail.com>
2023-08-09 01:41:31 +0100

    remove threading and multiprocess interactions
    keep threading only for files uploading

--- flask_demo.py
+++ flask_demo.py
@@ -1,40 +1,23 @@
-import time
-from concurrent.futures import ThreadPoolExecutor
-import json
 import os
-from multiprocessing.managers import BaseManager
+import time
+import uuid
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import as_completed
+
 from flask import Flask, request, jsonify, make_response, Response
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
-import uuid
-from concurrent.futures import as_completed
 from file_utils import ppt_preview
 from upload_s3 import upload_file_to_s3
+
+from index_server import DocumentManager
 
 app = Flask(__name__)
 app.response_buffering = False
 CORS(app)
 
-# initialize manager connection
-# TODO: you might want to handle the password in a less hardcoded way
-manager = BaseManager(("", 5602), b"password")
-manager.register("query_index")
-manager.register("insert_into_index")
-manager.register("get_documents_list")
-manager.register("create_index")
-manager.register("start_worker")
-manager.register("initialize_index")
-
-# Try to connect
-for _ in range(10):
-    try:
-        manager.connect()
-        break
-    except ConnectionRefusedError:
-        print("Connecting to index server has failed, waiting before retrying...")
-        time.sleep(3)
-
-executor = ThreadPoolExecutor()
+document_manager = DocumentManager()
+document_manager.initialize_index()
 
 
 @app.route("/stream")
@@ -47,25 +30,15 @@ def stream():
 
     if uuid_id is None:
         return "No text found, please include a ?text=blah parameter in the URL", 400
-    manager.initialize_index(uuid_id)
-    queue = manager.start_worker(query_text, uuid_id)
 
-    def generate():
-        while True:
-            response = (
-                queue.get()
-            )  # This will block until there is data available in the queue
-            if response is None:  # If we get None, that means the stream is done
-                break
-            yield str(response)
+    answer_stream = document_manager.query_stream(query_text, uuid_id)
 
-    return Response(generate(), mimetype="text/event-stream")
+    return Response(answer_stream, mimetype="text/event-stream")
 
 
 # TODO: Can we delete this route?
 @app.route("/query", methods=["GET"])
 def query_index():
-    global manager
     query_text = request.args.get("text", None)
     query_doc_id = request.args.get("doc_id", None)
     uuid_id = request.args.get("uuid", None)
@@ -74,8 +47,7 @@ def query_index():
     if uuid_id is None:
         return "No UUID found, please include a uuid in the URL", 400
 
-    manager.initialize_index(uuid_id)
-    response = manager.query_index(query_text, query_doc_id)._getvalue()
+    response = document_manager.query_index(query_text, query_doc_id)._getvalue()
     response_json = {
         "text": str(response),
     }
@@ -84,84 +56,91 @@ def query_index():
 
 @app.route("/uploadFile", methods=["POST"])
 def upload_file():
-    global manager, executor
     if "file" not in request.files:
         return "Please send a POST request with a file", 400
 
-    filepath = None
-    try:
-        generated_uuid = str(uuid.uuid4())
-        uploaded_file = request.files["file"]
-        filename = secure_filename(uploaded_file.filename)
-        filepath = os.path.join("documents", os.path.basename(filename))
+    with ThreadPoolExecutor as executor:
+        filepath = None
+        try:
+            generated_uuid = str(uuid.uuid4())
+            uploaded_file = request.files["file"]
+            filename = secure_filename(uploaded_file.filename)
+            filepath = os.path.join("documents", os.path.basename(filename))
+
+            start_time = time.time()
+            uploaded_file.save(filepath)
+            print("Saving the local PPT file: {:.2f}s".format(time.time() - start_time))
+
+            start_time = time.time()
+            if request.form.get("filename_as_doc_id", None) is not None:
+                document_manager.insert_into_index(filepath, doc_id=filename)
+            else:
+                document_manager.insert_into_index(filepath, generated_uuid)
+            print(
+                "Inserting into llama index: {:.2f}s".format(time.time() - start_time)
+            )
+        except Exception as e:
+            print(e)
+            # cleanup temp file
+            if filepath is not None and os.path.exists(filepath):
+                os.remove(filepath)
+            return "Error: {}".format(str(e)), 500
+
+        # upload file to s3
+        start_time = time.time()
+        upload_done = executor.submit(
+            upload_file_to_s3,
+            filepath,
+            "slidespeak-files",
+            generated_uuid + os.path.splitext(filepath)[1],
+        )
+        print("Upload PPT to S3: {:.2f}s".format(time.time() - start_time))
+
+        # delete file after upload
+        upload_done.add_done_callback(
+            lambda _: os.remove(filepath) if os.path.exists(filepath) else None
+        )
 
         start_time = time.time()
-        uploaded_file.save(filepath)
-        print('Saving the local PPT file: {:.2f}s'.format(time.time() - start_time))
+        preview_file_paths = ppt_preview(
+            filepath, "preview_images/" + generated_uuid + ".jpg"
+        )
+        print("Generating PPT preview: {:.2f}s".format(time.time() - start_time))
 
-        start_time = time.time()
-        if request.form.get("filename_as_doc_id", None) is not None:
-            manager.insert_into_index(filepath, doc_id=filename)
-        else:
-            manager.insert_into_index(filepath, generated_uuid)
-        print('Inserting into llama index: {:.2f}s'.format(time.time() - start_time))
-    except Exception as e:
-        print(e)
-        # cleanup temp file
-        if filepath is not None and os.path.exists(filepath):
-            os.remove(filepath)
-        return "Error: {}".format(str(e)), 500
+        preview_urls_dict = {}
 
-    # upload file to s3
-    start_time = time.time()
-    upload_done = executor.submit(
-        upload_file_to_s3,
-        filepath,
-        "slidespeak-files",
-        generated_uuid + os.path.splitext(filepath)[1],
-    )
-    print('Upload PPT to S3: {:.2f}s'.format(time.time() - start_time))
+        if len(preview_file_paths) > 0:
+            # Make a list of all futures for the uploads
+            future_to_preview = {
+                executor.submit(
+                    upload_file_to_s3,
+                    preview_file_path,
+                    "slidespeak-files",
+                    "preview-images/" + os.path.basename(preview_file_path),
+                ): preview_file_path
+                for preview_file_path in preview_file_paths
+            }
 
-    # delete file after upload
-    upload_done.add_done_callback(
-        lambda _: os.remove(filepath) if os.path.exists(filepath) else None
-    )
+            start_time = time.time()
+            for future in as_completed(future_to_preview):
+                preview_file_path = future_to_preview[future]
+                try:
+                    preview_url = future.result()
+                    index = preview_file_paths.index(preview_file_path)
+                    preview_urls_dict[index] = preview_url
 
-    start_time = time.time()
-    preview_file_paths = ppt_preview(
-        filepath, "preview_images/" + generated_uuid + ".jpg"
-    )
-    print('Generating PPT preview: {:.2f}s'.format(time.time() - start_time))
+                    if os.path.exists(preview_file_path):
+                        os.remove(preview_file_path)
+                except Exception as exc:
+                    print(f"{preview_file_path} generated an exception: {exc}")
+            print(
+                "Uploading preview images to S3: {:.2f}s".format(
+                    time.time() - start_time
+                )
+            )
 
-    preview_urls_dict = {}
-
-    if len(preview_file_paths) > 0:
-        # Make a list of all futures for the uploads
-        future_to_preview = {
-            executor.submit(
-                upload_file_to_s3,
-                preview_file_path,
-                "slidespeak-files",
-                "preview-images/" + os.path.basename(preview_file_path)
-            ): preview_file_path for preview_file_path in preview_file_paths
-        }
-
-        start_time = time.time()
-        for future in as_completed(future_to_preview):
-            preview_file_path = future_to_preview[future]
-            try:
-                preview_url = future.result()
-                index = preview_file_paths.index(preview_file_path)
-                preview_urls_dict[index] = preview_url
-
-                if os.path.exists(preview_file_path):
-                    os.remove(preview_file_path)
-            except Exception as exc:
-                print(f'{preview_file_path} generated an exception: {exc}')
-        print('Uploading preview images to S3: {:.2f}s'.format(time.time() - start_time))
-
-    # Convert dict to list in correct order
-    preview_urls = [preview_urls_dict[i] for i in sorted(preview_urls_dict.keys())]
+        # Convert dict to list in correct order
+        preview_urls = [preview_urls_dict[i] for i in sorted(preview_urls_dict.keys())]
 
     return (
         make_response(jsonify({"uuid": generated_uuid, "previewUrls": preview_urls})),
@@ -171,8 +150,7 @@ def upload_file():
 
 @app.route("/getDocuments", methods=["GET"])
 def get_documents():
-    document_list = manager.get_documents_list()._getvalue()
-
+    document_list = document_manager.get_documents_list()
     return make_response(jsonify(document_list)), 200
 
 
